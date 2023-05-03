ee2d9d42da1ef001e6c44401dac206ca04c6a9d7
EWMApp <100062013+ewmapp@users.noreply.github.com>
2023-05-02 23:51:17 -0300

    refacture

--- server-chat.js
+++ server-chat.js
@@ -2,21 +2,23 @@
 const http = require('http')
 const { Server } = require('socket.io')
 const mysql = require('mysql2')
+const path = require('path')
+const dotenv = require('dotenv').config({ path: './.env' })
 
 // Configurações do banco de dados
 const dbConfig = {
-  host: 'db-mysql-nyc1-chat-do-user-2678382-0.b.db.ondigitalocean.com',
-  user: 'smartht',
-  password: 'AVNS_1Om-Zz4iBSsKNY0UXR9',
-  database: 'defaultdb',
-  port: 25060
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 }
 
 // Cria a conexão com o banco de dados
 const db = mysql.createConnection(dbConfig)
 
 // Cria o servidor HTTP e passa-o para o construtor Server do socket.io
-const port = process.env.PORT || 50002
+const port = process.env.PORT
 const server = http.createServer()
 const io = new Server(server, {
   cors: {
@@ -37,60 +39,122 @@ db.query(`
   )
 `)
 
-const rooms = new Map()
-
-io.on('connection', socket => {
-  console.log('Novo usuário conectado')
-
-  // Quando o cliente entra em uma nova sala
-  socket.on('joinRoom', ({ author_id, author_name, room }) => {
-    console.log(`Usuário ${author_name} entrou na sala ${room}`)
-    // Verifica se já existe um usuário com o mesmo author_id na sala
+// Funções auxiliares
+async function getUserByAuthorIdAndRoom(author_id, room) {
+  return new Promise((resolve, reject) => {
     db.query(
       'SELECT * FROM users WHERE author_id = ? AND room = ?',
       [author_id, room],
       (err, results) => {
         if (err) {
-          console.error(err)
+          reject(err)
           return
         }
+        resolve(results)
+      }
+    )
+  })
+}
 
-        // Remove o usuário anterior (se existir)
-        if (results.length > 0) {
-          /* db.query('DELETE FROM users WHERE socket_id = ?', [
-            results[0].socket_id
-          ])
-          socket.to(room).emit('userLeft', {
-            author_id: results[0].author_id,
-            author_name: results[0].author_name,
-            author_msg: 'duas conexões simultâneas'
-          }) */
-          console.log('Usuário já conectado na sala')
+async function addUserToDatabase(author_id, author_name, room, socket_id) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'INSERT INTO users (author_id, author_name, room, socket_id) VALUES (?, ?, ?, ?)',
+      [author_id, author_name, room, socket_id],
+      (err, results) => {
+        if (err) {
+          reject(err)
+          return
         }
+        resolve(results)
+      }
+    )
+  })
+}
 
-        // Adiciona o usuário ao banco de dados
-        db.query(
-          'INSERT INTO users (author_id, author_name, room, socket_id) VALUES (?, ?, ?, ?)',
-          [author_id, author_name, room, socket.id]
-        )
+async function removeUserFromRoom(socket_id, room) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'DELETE FROM users WHERE socket_id = ? AND room = ?',
+      [socket_id, room],
+      (error, results) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve(results)
+      }
+    )
+  })
+}
 
-        // O cliente entra na sala
+async function removeUserFromDatabase(socket_id) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'DELETE FROM users WHERE socket_id = ?',
+      [socket_id],
+      (error, results) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve(results)
+      }
+    )
+  })
+}
+
+async function countUserFromRoom(room) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'SELECT COUNT(distinct author_id) as total FROM users WHERE room = ?',
+      [room],
+      (error, results) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve(results)
+      }
+    )
+  })
+}
+
+const rooms = new Map()
+
+io.on('connection', socket => {
+  // Quando o cliente entra em uma nova sala
+  socket.on('joinRoom', async ({ author_id, author_name, room }) => {
+    console.log(`Usuário ${author_name} entrou na sala ${room}`)
+    // Verifica se já existe um usuário com o mesmo author_id na sala
+    try {
+      const existingUser = await getUserByAuthorIdAndRoom(author_id, room)
+      if (existingUser.length > 0) {
+        console.log('Usuário já conectado na sala')
+      } else {
+        await addUserToDatabase(author_id, author_name, room, socket.id)
         socket.join(room)
-
-        // Notifica a sala que um novo usuário entrou
         socket.to(room).emit('userJoined', {
           author_id: author_id,
           author_name: author_name,
           author_msg: 'conectou-se'
         })
       }
-    )
+    } catch (err) {
+      console.error(err)
+    }
   })
 
   // Quando o cliente envia uma mensagem
   socket.on('newMessage', ({ author_id, author_name, author_msg, room }) => {
+    // Envia a mensagem para a sala
+    /* io.to(room).emit('receivedMessage', {
+      author_id: author_id,
+      author_name: author_name,
+      author_msg: author_msg
+    }) */
+
     // Encontra o usuário que enviou a mensagem
-    console.log(`Usuário ${author_name} enviou uma mensagem na sala ${room}`)
     db.query(
       'SELECT * FROM users WHERE socket_id = ?',
       [socket.id],
@@ -99,7 +163,6 @@ io.on('connection', socket => {
           console.error(err)
           return
         }
-
         // Envia a mensagem para a sala
         io.to(room).emit('receivedMessage', {
           author_id: results[0].author_id,
@@ -111,27 +174,21 @@ io.on('connection', socket => {
   })
 
   // Define o evento de saída de uma sala
-  socket.on('leaveRoom', room => {
+  socket.on('leaveRoom', async room => {
     // Remove o usuário do banco de dados
-    db.query(
-      'DELETE FROM users WHERE socket_id = ?',
-      [socket.id],
-      (error, results) => {
-        if (error) {
-          console.error(error)
-          return
-        }
-      }
-    )
+    try {
+      await removeUserFromRoom(socket.id, room)
+    } catch (err) {
+      console.error(err)
+    }
     // Sai da sala
     socket.leave(room)
-
     // Envia uma mensagem para a sala informando que um usuário saiu
     socket.to(room).emit('userLeave', `Usuário saiu da sala`)
   })
 
   // Quando o cliente se desconecta
-  socket.on('disconnect', () => {
+  socket.on('disconnect', async () => {
     // Notifica a sala que o usuário saiu
     db.query(
       'SELECT * FROM users WHERE socket_id = ?',
@@ -141,7 +198,6 @@ io.on('connection', socket => {
           console.error(error)
           return
         }
-
         if (results.length > 0) {
           const user = results[0]
           io.to(user.room).emit('userLeft', {
@@ -152,37 +208,24 @@ io.on('connection', socket => {
         }
       }
     )
-
     // Remove o usuário do banco de dados
-    db.query(
-      'DELETE FROM users WHERE socket_id = ?',
-      [socket.id],
-      (error, results) => {
-        if (error) {
-          console.error(error)
-          return
-        }
-      }
-    )
+    try {
+      await removeUserFromDatabase(socket.id)
+    } catch (err) {
+      console.error(err)
+    }
   })
 
   // Total de usuários online na sala
-  socket.on('totalUsers', room => {
-    // Encontra o usuário que enviou a mensagem
-    db.query(
-      'SELECT COUNT(distinct author_id) as total FROM users WHERE room = ?',
-      [room],
-      (err, results) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-
-        // Envia a mensagem para a sala
-        io.to(room).emit('onlineRoom', results[0].total)
-        console.log(results)
-      }
-    )
+  socket.on('totalUsers', async room => {
+    // Total de usuários na sala
+    console.log('totalUsers')
+    try {
+      const res = await countUserFromRoom(room)
+      io.to(room).emit('onlineRoom', res[0].total)
+    } catch (err) {
+      console.error(err)
+    }
   })
 })
 
