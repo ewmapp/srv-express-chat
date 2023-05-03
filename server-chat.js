e2464a2c3e994aaa3cbcb446b95c90f22a548cf8
EWMApp <100062013+ewmapp@users.noreply.github.com>
2023-05-02 21:22:13 -0300

    Update orign

--- server-chat.js
+++ server-chat.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 50002
 const server = http.createServer()
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://conferencialivresvsa.com.br',
     methods: ['GET', 'POST']
   }
 })
